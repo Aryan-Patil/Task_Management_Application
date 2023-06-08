fa0a562927e7487ca1afeba0738703aefafe3850
Aryan Patil <79255053+Aryan-Patil@users.noreply.github.com>
2023-06-08 23:21:51 +0530

    forgot password page design completed

--- src/Components/ForgotPassword.js
+++ src/Components/ForgotPassword.js
@@ -1,9 +1,37 @@
 import React from 'react'
-
+import { Player } from '@lottiefiles/react-lottie-player';
+import './CSS/forgotpass.css'
+import { NavLink } from 'react-router-dom';
 function ForgotPassword() {
-  return (
-    <div>ForgotPassword</div>
-  )
+    return (
+        <div className='forgot-pass-main'><div className="forgot-pass-card">
+            <div className="forgot-pass-animation">
+                <Player
+                    className='animation'
+                    autoplay
+                    loop
+                    src="https://assets4.lottiefiles.com/packages/lf20_mdhnmscc.json"
+                />
+                <div className="straight-line"></div>
+            </div>
+            <div className="login-form">
+                <div className="heading">
+                    Forgot Password
+                </div>
+                <form >
+                    <div className="form-child">
+                        <input type="email"  required placeholder="Email" />
+                    </div>
+                    <input className='submit-btn' type="submit" value="Reset my Password" />
+                </form>
+                <div className="go-to-login">
+                    <span>Back to </span>
+                    <NavLink className='color-2' to='/'>Login</NavLink>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
 }
 
 export default ForgotPassword
